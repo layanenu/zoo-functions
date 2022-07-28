725c623eaa0d8bf1b3da9193bfecb121065242e5
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-28 17:35:51 -0300

    requisitos 6, 7 e 15 resolvidos

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -2,33 +2,35 @@ const { species } = require('../data/zoo_data');
 
 const getElephants = () =>
   species.find((specie) => specie.name === 'elephants');
+  // retorna o elemento elephants, ou seja, o objeto elephants (que é o primeiro elemento desse array que satisfaz essa condição)
 
 const averageAge = ({ residents }) =>
   residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
+  // retorna a media de idades (1 único número)
 
 const computeData = (param, elephants) => {
   switch (param) {
   case 'count':
-    return elephants.residents.length;
+    return elephants.residents.length; // retorna a quantidade de elefantes
   case 'names':
-    return elephants.residents.map((elephant) => elephant.name);
+    return elephants.residents.map((elephant) => elephant.name); // retorna um ARRAY com a relação dos nomes de todos os elefantes
   case 'averageAge':
-    return averageAge(elephants);
+    return averageAge(elephants); // retorna a media de idades
   default:
     return null;
   }
 };
 
-const handlerElephants = (param) => {
-  if (param === undefined) {
+const handlerElephants = (param) => { // essa funcao serve para localizar os valores das chaves que o usuário quer, que deve ser escrito como string
+  if (param === undefined) { // se o parametro for undefined - retorna undefined
     return undefined;
   }
-  if (typeof param !== 'string') {
+  if (typeof param !== 'string') { // se o parametro for diferente de string - retorna string
     return 'Parâmetro inválido, é necessário uma string';
   }
-  const elephants = getElephants();
-  if (Object.keys(elephants).includes(param)) {
-    return elephants[param];
+  const elephants = getElephants(); // essa constante chama a funcao getElephants que usa o FIND para RETORNAR o OBJETO que contém os dados do elephant
+  if (Object.keys(elephants).includes(param)) { // se dentro das CHAVES do OBJETO elephant estiver INCLUIDO o que foi pedido no parametro
+    return elephants[param]; // RETORNA aquele parametro
   }
   return computeData(param, elephants);
 };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se para o parâmetro count retorna o número inteiro 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica se retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verifica se retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Verifica se retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verifica se retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Verifica se retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Verifica se não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica se passando para o parâmetro um objeto vazio retorna a frase: parâmetro inválido, é necessário uma string', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se passada uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('teste')).toBeNull();
+  });
+});
