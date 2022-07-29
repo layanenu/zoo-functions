dfda5bdfd171db1e50f5c49da18346131db982b7
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-29 17:24:57 -0300

    requisito 2 resolvido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.some((element) => {
+    if (element.name === animal) {
+      return element.residents.every((element2) => element2.age >= age);
+    }
+    return false;
+  });
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
