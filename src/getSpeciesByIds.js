4d2e8c1801c48225abbc963244dfe1793ea7e947
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-29 14:56:06 -0300

    requisito 1 resolvido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  const filtraAnimalPeloId = species.filter((animal) => ids.includes(animal.id));
+  return filtraAnimalPeloId;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
