a92d001024d381a4f6d2fc626728461866cfb36f
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-29 15:03:39 -0300

    requisito 1 resolvido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,4 @@ function getSpeciesByIds(...ids) {
   return filtraAnimalPeloId;
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
-
 module.exports = getSpeciesByIds;
