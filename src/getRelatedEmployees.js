6b1615c7972b902c3d292b2a16a9e4839ffef2aa
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-30 12:46:21 -0300

    requisito 4 resolvido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(id) { // verificar se o colaborador é gerente
+  return data.employees.some((element) => (element.managers.includes(id)));
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(managerId) { // retornar as pessoas lideradas pela gerencia
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const pessoasLideradas = data.employees.filter((element) => element.managers.includes(managerId)); // filtra as pessoas lideradas
+  const fullNamePessoasLideradas = pessoasLideradas
+    .map((element) => `${element.firstName} ${element.lastName}`); // usa o map para retornar só o fullName
+  return fullNamePessoasLideradas;
 }
 
 module.exports = { isManager, getRelatedEmployees };
