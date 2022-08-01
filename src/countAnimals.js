867ec598ae2b156a615613b03cb5e2ca294cb995
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-01 12:29:26 -0300

    requisito 5 resolvido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal) { // contar a quantidade de animais que residem no zoológico.
+  if (!animal) {
+    const allSpecies = {};
+    data.species.forEach((especies) => {
+      allSpecies[especies.name] = especies.residents.length;
+    });
+    return allSpecies;
+  }
+  const filtraAnimal = data.species.filter((element) => element.name === animal.specie);
+  if (animal.sex) {
+    return filtraAnimal[0].residents.filter((element2) => element2.sex === animal.sex).length;
+  }
+  return filtraAnimal[0].residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
