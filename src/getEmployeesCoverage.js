3dbf7b103319c25f5f8b836338291c3cc41afddd
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-01 22:02:52 -0300

    requisito 11 resolvido

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,16 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage(nameOuId) { // retornar as informações sobre a pessoa colaboradora e por quais espécies ela é responsável.
-  if (!nameOuId) {
-    const employeerInfo = {};
-    data.employees.forEach((element) => {
-      // employeerInfo[element.id] = element.id;
-      employeerInfo[element.fullName] = element.firstName + element.lastName;
-    });
-    return employeerInfo;
+function renderEmployeer(objeto) {
+  return {
+    id: objeto.id,
+    fullName: `${objeto.firstName} ${objeto.lastName}`,
+    species: objeto.responsibleFor.map((element) => {
+      const specieObject = data.species.find((specie) => specie.id === element);
+      return specieObject.name;
+    }),
+    locations: objeto.responsibleFor.map((element) => {
+      const specieLocation = data.species.find((localizacao) => localizacao.id === element);
+      return specieLocation.location;
+    }),
+  };
+}
+function findEmployeerById(id) {
+  const objeto = data.employees.find((element) => element.id === id);
+  if (!objeto) {
+    throw new Error('Informações inválidas');
   }
+  return renderEmployeer(objeto);
+}
+function findEmployeerByName(name) {
+  const objeto = data.employees
+    .find((element) => element.firstName === name || element.lastName === name);
+  if (!objeto) {
+    throw new Error('Informações inválidas');
+  }
+  return renderEmployeer(objeto);
 }
 
-console.log(getEmployeesCoverage());
+function getEmployeesCoverage(nameOuId) {
+  if (nameOuId && nameOuId.id) {
+    return findEmployeerById(nameOuId.id);
+  }
+
+  if (nameOuId && nameOuId.name) {
+    return findEmployeerByName(nameOuId.name);
+  }
+
+  return data.employees.map((employee) => renderEmployeer(employee));
+}
 
 module.exports = getEmployeesCoverage;
