a54d6a905a09be50b403e12671988767a6ae2119
Layane Nunes <layanenu@gmail.com>
2023-05-10 12:26:53 -0300

    feat: funcao getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,56 @@
 const data = require('../data/zoo_data');
 
+const animal = data.species.map((especie) => especie.name);
+const dias = Object.keys(data.hours);
+
+function Animal(param) {
+  return data.species.find((especie) => especie.name === param).availability;
+}
+
+function diasDeFuncionamento(dia) {
+  const agenda = {};
+  if (dia === 'Monday') {
+    agenda[dia] = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  } else {
+    agenda[dia] = {
+      officeHour: `Open from ${data.hours[dia].open}am until ${data.hours[dia].close}pm`,
+      exhibition: data.species.filter((animais) => animais.availability.includes(dia))
+        .map((animais) => animais.name),
+    };
+  }
+  return agenda;
+}
+
+function diasDeFuncionamentoDaSemana() {
+  const agenda = {};
+  dias.forEach((dia) => {
+    if (dia === 'Monday') {
+      agenda[dia] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      agenda[dia] = {
+        officeHour: `Open from ${data.hours[dia].open}am until ${data.hours[dia].close}pm`,
+        exhibition: data.species.filter((animais) => animais.availability.includes(dia))
+          .map((animais) => animais.name),
+      };
+    }
+  });
+  return agenda;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (animal.includes(scheduleTarget)) {
+    return Animal(scheduleTarget);
+  }
+  if (dias.includes(scheduleTarget)) {
+    return diasDeFuncionamento(scheduleTarget);
+  }
+  return diasDeFuncionamentoDaSemana();
 }
 
 module.exports = getSchedule;
